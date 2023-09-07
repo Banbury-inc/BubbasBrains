dc0479eb88877eed27a430c9abac77342a83a3b1
mmills <mmills6060@gmail.com>
2023-09-07 13:21:03 -0400

    working live object detection

---
+++ ultralytics_working.py
@@ -0,0 +1,14 @@
+
+
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n.pt')  # load an official detection model
+# model = YOLO('yolov8n-seg.pt')  # load an official segmentation model
+#model = YOLO('path/to/best.pt')  # load a custom model
+
+# Track with the model
+# results = model(source="https://youtu.be/Zgi9g1ksQHc", show=True, conf=0.4, save=True)
+
+
+results = model(source=0, show=True, conf=0.4, save=True)
