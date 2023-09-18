e0b8e023ef95ab34006776cde3ba0d7a82d84eb2
mmills <mmills6060@gmail.com>
2023-09-18 14:50:20 -0400

    app.py

--- api/app.py
+++ api/app.py
@@ -7,10 +7,17 @@ import platform
 import json
 import cv2
 from ultralytics import YOLO
+import threading
+
 
 
 app = Flask(__name__)
 CORS(app)
+
+# Create a lock for thread synchronization
+lock = threading.Lock()
+
+
 @app.route("/")
 def index():
     return "Extensions that can be used: <br> <br> /helloworld <br> /getjetsonmodel <br> /getdeviceinfo <br> /videostream"
@@ -99,18 +106,19 @@ def generate_frames():
             raise Exception("Could not open camera.")
 
         while True:
-            # Capture frame-by-frame
-            ret, frame = cap.read()
+            with lock:
+                # Capture frame-by-frame
+                ret, frame = cap.read()
 
-            if not ret:
-                break
+                if not ret:
+                    break
 
-            # Encode the frame as JPEG
-            _, buffer = cv2.imencode('.jpg', frame)
+                # Encode the frame as JPEG
+                _, buffer = cv2.imencode('.jpg', frame)
 
-            # Yield the frame as bytes
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+                # Yield the frame as bytes
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
     except Exception as e:
         print("Error:", str(e))
@@ -137,24 +145,96 @@ def generate_frames_with_object_detection():
         success, frame = cap.read()
 
         if success:
-            # Run YOLOv8 inference on the frame
-            results = model(frame)
+            with lock:
+                # Run YOLOv8 inference on the frame
+                results = model(frame)
 
-            # Visualize the results on the frame
-            annotated_frame = results[0].plot()
+                # Visualize the results on the frame
+                annotated_frame = results[0].plot()
 
-            # Encode the frame as JPEG
-            _, buffer = cv2.imencode('.jpg', annotated_frame)
+                # Encode the frame as JPEG
+                _, buffer = cv2.imencode('.jpg', annotated_frame)
 
-            # Yield the frame as bytes
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+                # Yield the frame as bytes
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
         
 @app.route("/od-videostream")
 def video_stream_with_object_detection():
     return Response(generate_frames_with_object_detection(), mimetype='multipart/x-mixed-replace; boundary=frame')
+#-------------------------------------------------------------
 
+
+current_video_source = "http://127.0.0.1:5000/videostream-update"  # Initial video source
+
+# ... (Your existing route functions)
+
+@app.route("/videostream-update")
+def video_stream():
+    global current_video_source  # Use the global variable for video source
+
+    def generate_frames():
+        try:
+            cap = cv2.VideoCapture(current_video_source)  # Use the current video source
+
+            if not cap.isOpened():
+                raise Exception("Could not open camera.")
+
+            while True:
+                ret, frame = cap.read()
+
+                if not ret:
+                    break
+
+                _, buffer = cv2.imencode('.jpg', frame)
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+        except Exception as e:
+            print("Error:", str(e))
+        finally:
+            cap.release()
+
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Update the video source and restart the stream for object detection
+@app.route("/od-videostream-update")
+def video_stream_with_object_detection():
+    global current_video_source  # Use the global variable for video source
+
+    def generate_frames_with_object_detection():
+        try:
+            cap = cv2.VideoCapture(current_video_source)  # Use the current video source
+
+            if not cap.isOpened():
+                raise Exception("Could not open camera.")
+
+            while True:
+                success, frame = cap.read()
+
+                if success:
+                    results = model(frame)
+                    annotated_frame = results[0].plot()
+                    _, buffer = cv2.imencode('.jpg', annotated_frame)
+                    yield (b'--frame\r\n'
+                           b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+        except Exception as e:
+            print("Error:", str(e))
+        finally:
+            cap.release()
+
+    return Response(generate_frames_with_object_detection(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Update the current video source when requested
+@app.route("/update-video-source", methods=["POST"])
+def update_video_source():
+    global current_video_source
+    data = request.get_json()
+    new_source = data.get("source")
+    current_video_source = new_source
+    return jsonify({"message": "Video source updated successfully"})
 
 if __name__ == "__main__":
     app.run(debug=True)
