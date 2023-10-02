affae3ee550e1dcf267f386c81d3f41f65a2f459
Michael Mills <mmills6060@gmail.com>
2023-10-02 12:38:44 -0400

    changing main

--- Motor_Control/main.py
+++ Motor_Control/main.py
@@ -3,14 +3,32 @@ import time
 import L300n
 import L400n
 def main():
-    # call function L300n which is in another file
-    while True:
-        L300n.L300n()
-        # Set a timer for 5 seconds
-        time.sleep(5)
-        L400n.L400n()
 
-        time.sleep(5)
+    serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
+    baud_rate = 9600  # Adjust this to match your device's baud rate
+
+
+    ser = None  # Initialize ser outside of the try block
+
+
+    ser = serial.Serial(serial_port, baud_rate)
+
+    print(f"Connected to {serial_port} at {baud_rate} baud")
+
+
+    user_input = "L400n"
+
+
+    ser.write(user_input.encode('utf-8'))
+
+
+    time.sleep(5)
+
+
+    ser.close()
+
+
+    print("Serial port closed")
 
 if __name__ == "__main__":
     main()
