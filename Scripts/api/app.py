f1c00d24dfdc96f6ccb319d4a0ef898d7c68f37f
Michael Mills <mmills6060@gmail.com>
2023-10-02 15:16:15 -0400

    trying new things

--- Scripts/api/app.py
+++ Scripts/api/app.py
@@ -65,17 +65,45 @@ def stop_response():
     return Response(stop(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def up():
-    serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
-    baud_rate = 9600  # Adjust this to match your device's baud rate
-    ser = None  # Initialize ser outside of the try block
-    ser = serial.Serial(serial_port, baud_rate)
-    print(f"Connected to {serial_port} at {baud_rate} baud")
-    print("Moving forward")
     while True:
-        user_input = "L400n"
-        ser.write(user_input.encode('utf-8'))
+        serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
+        baud_rate = 9600  # Adjust this to match your device's baud rate
+        ser = None  # Initialize ser outside of the try block
+        ser = serial.Serial(serial_port, baud_rate)
+        print(f"Connected to {serial_port} at {baud_rate} baud")
+        count = 0
+        while count < 3000:
+            user_input = "L400n"
+            ser.write(user_input.encode('utf-8'))
+            count = count + 1
+            print(count)
+        print("Timer finished, closing port")
+        ser.close()
+        print("Serial port closed")
+        serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
+        baud_rate = 9600  # Adjust this to match your device's baud rate
+        ser = None  # Initialize ser outside of the try block
+
+        ser = serial.Serial(serial_port, baud_rate)
+
+        print(f"Connected to {serial_port} at {baud_rate} baud")
+        count = 0
+        while count < 3000:
+            user_input = "L300n"
+        
+
+            ser.write(user_input.encode('utf-8'))
+
+            count = count + 1
+            
+            print(count)
 
 
+        print("Timer finished, closing port")
+        ser.close()
+
+
+        print("Serial port closed")
 
 @app.route("/up")
 def up_response():
