0284140a8e93cefe2e92cefa3238288765d2a583
Michael Mills <mmills6060@gmail.com>
2023-10-02 15:34:56 -0400

    editing timer

--- Scripts/api/app.py
+++ Scripts/api/app.py
@@ -67,7 +67,7 @@ def stop_response():
 def up():
     
     count = 0
-    while count < 1000:
+    while count < 10:
         serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
         baud_rate = 9600  # Adjust this to match your device's baud rate
         ser = None  # Initialize ser outside of the try block
@@ -78,6 +78,7 @@ def up():
             user_input = "L400n"
             ser.write(user_input.encode('utf-8'))
             secondcount = secondcount + 1
+            print(secondcount)
             print(count)
         print("Timer finished, closing port")
         count = count + 1
