93fc180ac1370873cb0165ac05c7b1fd2be6177d
Michael Mills <mmills6060@gmail.com>
2023-10-02 12:58:11 -0400

    adding a different speed

--- Motor_Control/main.py
+++ Motor_Control/main.py
@@ -32,5 +32,34 @@ def main():
 
     print("Serial port closed")
 
+    serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
+
+    baud_rate = 9600  # Adjust this to match your device's baud rate
+
+
+    ser = None  # Initialize ser outside of the try block
+
+
+    ser = serial.Serial(serial_port, baud_rate)
+
+    print(f"Connected to {serial_port} at {baud_rate} baud")
+    count = 0
+    while count < 3000:
+        user_input = "L300n"
+    
+
+        ser.write(user_input.encode('utf-8'))
+
+        count = count + 1
+        
+        print(count)
+
+
+    print("Timer finished, closing port")
+    ser.close()
+
+
+    print("Serial port closed")
+
 if __name__ == "__main__":
     main()
