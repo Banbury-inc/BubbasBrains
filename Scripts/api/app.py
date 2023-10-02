8679bae88ada7f739e25306f1e66ad19d798a5e4
Michael Mills <mmills6060@gmail.com>
2023-10-02 13:26:28 -0400

    commenting out ultralytics which seems to be broken

--- Scripts/api/app.py
+++ Scripts/api/app.py
@@ -2,53 +2,22 @@ from flask import Flask, Response
 import cv2
 from waitress import serve
 import threading
-from ultralytics import YOLO
+# from ultralytics import YOLO
 import serial.tools.list_ports
 #import drivers
 import time
 
 
-app = Flask(__name__)
+app = Flask(__name__ )
 
 # Initialize the YOLO model
-model = YOLO('yolov8n.pt')
+# model = YOLO('yolov8n.pt')
 
 # Create a lock for thread synchronization
 lock = threading.Lock()
 
 # Initialize the camera_run flag
 camera_run = False
-def setup_COM():
-
-    # Define the serial port and baud rate
-    serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
-    baud_rate = 9600  # Adjust this to match your device's baud rate
-
-    ser = None  # Initialize ser outside of the try block
-
-    try:
-        # Open the serial port
-        ser = serial.Serial(serial_port, baud_rate)
-        print(f"Connected to {serial_port} at {baud_rate} baud")
-
-        while True:
-            # Get user input and send it to the serial port
-            user_input = input("Enter data to send (or press Enter to quit): ")
-            if user_input:
-                ser.write(user_input.encode('utf-8'))  # Encode the string as bytes
-
-    except serial.SerialException as e:
-        print(f"Error: {e}")
-    finally:
-        # Close the serial port when done
-        if ser is not None and ser.is_open:
-            ser.close()
-            print("Serial port closed")
-
-@app.route("/setupCOM")
-def setupCOM_response():
-    return Response(setup_COM(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 def stop():
    print("Stopped")
