ccd7112444486655a28fbf8f364de0bfc164b5c6
Michael Mills <mmills6060@gmail.com>
2023-10-03 11:45:07 -0400

    global serial

--- Scripts/api/app.py
+++ Scripts/api/app.py
@@ -102,6 +102,7 @@ def up1():
     print("Moving forward at speed 1")
     count = 0
     while count < 1:
+        global ser
         serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
         baud_rate = 9600  # Adjust this to match your device's baud rate
         ser = None  # Initialize ser outside of the try block
@@ -127,11 +128,6 @@ def up2():
     print("Moving forward at speed 2")
     count = 0
     while count < 1:
-        serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
-        baud_rate = 9600  # Adjust this to match your device's baud rate
-        ser = None  # Initialize ser outside of the try block
-        ser = serial.Serial(serial_port, baud_rate)
-        print(f"Connected to {serial_port} at {baud_rate} baud")
         secondcount = 0 
         while secondcount < 100:
             user_input = "L300n"
