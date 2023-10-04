8a0473659ed807c9bdf1a9c89003721475975de4
Michael Mills <mmills6060@gmail.com>
2023-10-04 15:25:10 -0400

    yes

--- Scripts/api/app.py
+++ Scripts/api/app.py
@@ -102,7 +102,6 @@ def forward2_left0():
         global ser
         serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
         baud_rate = 9600  # Adjust this to match your device's baud rate
-        ser = None  # Initialize ser outside of the try block
         ser = serial.Serial(serial_port, baud_rate)
         print(f"Connected to {serial_port} at {baud_rate} baud")
         secondcount = 0 
@@ -233,7 +232,6 @@ def forward1_left0():
         global ser
         serial_port = '/dev/ttyUSB0'  # Adjust this to match your serial port
         baud_rate = 9600  # Adjust this to match your device's baud rate
-        ser = None  # Initialize ser outside of the try block
         ser = serial.Serial(serial_port, baud_rate)
         print(f"Connected to {serial_port} at {baud_rate} baud")
         secondcount = 0 
