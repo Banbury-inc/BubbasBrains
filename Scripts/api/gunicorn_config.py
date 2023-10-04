43e2f6d784a03594a6a8c96ce72f9991d86550e1
Michael Mills <mmills6060@gmail.com>
2023-10-04 16:01:17 -0400

    trying things

--- Scripts/api/gunicorn_config.py
+++ Scripts/api/gunicorn_config.py
@@ -1,3 +1,3 @@
-workers = 4  # Adjust the number of workers as needed
+workers = 8  # Adjust the number of workers as needed
 bind = "192.168.1.51:4001"  # Replace with your desired host and port
 timeout = 600
