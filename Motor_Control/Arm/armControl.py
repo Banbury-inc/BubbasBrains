b0a4010135ec8a9e92d89892dd75980d80ddce88
Michael Mills <mmills6060@gmail.com>
2023-10-11 14:46:13 -0400

    dwqw

--- Motor_Control/Arm/armControl.py
+++ Motor_Control/Arm/armControl.py
@@ -3,3 +3,8 @@ from adafruit_servokit import ServoKit
 
 
 kit = ServoKit(channels=16, address=0x77)
+kit = ServoKit(channels=16, address=0x71)
+kit = ServoKit(channels=16, address=0x72)
+kit = ServoKit(channels=16, address=0x72)
+kit = ServoKit(channels=16, address=0x50)
+kit = ServoKit(channels=16, address=0x57)
