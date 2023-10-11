70687cba734a1c2cf9f725c641a486706864305b
Michael Mills <mmills6060@gmail.com>
2023-10-11 14:01:22 -0400

    removing stuff

--- Motor_Control/Arm/armControl.py
+++ Motor_Control/Arm/armControl.py
@@ -1,15 +1,5 @@
 import time
 from adafruit_servokit import ServoKit
 
-kit = ServoKit(channels=16)
-armStart = "startup"
-
-def armInit():
-    kit.servo[2].angle = 90
-    time.sleep(1)
 
 
-
-while True :
-    if armStart ==  "startup":
-        armStrart = armInit()
