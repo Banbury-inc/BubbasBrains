8201bdf8518c11fb1394239d7ef9228d9fa32d47
Michael Mills <mmills6060@gmail.com>
2023-10-11 15:40:34 -0400

    editing

--- Motor_Control/Arm/armControl.py
+++ Motor_Control/Arm/armControl.py
@@ -1,35 +1,10 @@
-# SDA = pin.SDA_1
-# SCL = pin.SCL_1
-# SDA_1 = pin.SDA
-# SCL_1 = pin.SCL
 
-from adafruit_servokit import ServoKit
-import board
-import busio
 import time
+from adafruit_servokit import ServoKit
 
+# Connect to the I2C bus and address
+i2c_bus_number = 1  # Adjust this to the correct I2C bus number
+device_address = 0x71  # Adjust this to the correct device address
 
-# On the Jetson Nano
-# Bus 0 (pins 28,27) is board SCL_1, SDA_1 in the jetson board definition file
-# Bus 1 (pins 5, 3) is board SCL, SDA in the jetson definition file
-# Default is to Bus 1; We are using Bus 0, so we need to construct the busio first ...
-print("Initializing Servos")
-i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
-print("Initializing ServoKit")
-kit = ServoKit(channels=16, i2c=i2c_bus0)
-# kit[0] is the bottom servo
-# kit[1] is the top servo
-print("Done initializing")
-sweep = range(0,180)
-for degree in sweep :
-    kit.servo[0].angle=degree
-    # kit.servo[1].angle=degree
-    # time.sleep(0.01)
-
-time.sleep(0.5)
-sweep = range(180,0, -1)
-for degree in sweep :
-    kit.servo[0].angle=degree
-    
-last_presses = None
-           
+# Initialize the ServoKit with the specified I2C bus and address
+kit = ServoKit(channels=16, address=device_address, i2c=i2c_bus_number)
