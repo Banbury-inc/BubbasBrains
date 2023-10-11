f4b4a2d36c31386987f70dd3ce68baa0dc25e437
Michael Mills <mmills6060@gmail.com>
2023-10-11 12:02:25 -0400

    arm control

---
+++ Motor_Control/Arm/armControl.py
@@ -0,0 +1,16 @@
+import time
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channels=16)
+armStart = "startup"
+
+def armInit():
+    kit.servo[2].angle = 90
+    time.sleep(1)
+    kit.servo[3].angle = 90
+    time.sleep(1)
+
+
+while True :
+    if armStart ==  "startup":
+        armStrart = armInit()
