fe7fefadbc57d61bcf99bcc3c29f7aba605bc5d2
Michael Mills <mmills6060@gmail.com>
2023-10-11 12:29:13 -0400

    for i in range

--- Motor_Control/Arm/armControl.py
+++ Motor_Control/Arm/armControl.py
@@ -7,8 +7,11 @@ armStart = "startup"
 def armInit():
     for i in range(16):
 
-        kit.servo[2].angle = 90
+        kit.servo[i].angle = 90
         time.sleep(1)
+        kit.servo[i].angle = 90
+        time.sleep(1)
+
 
 
 while True :
