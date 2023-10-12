9ab188e51c6157cc3c80586a09abcbb604b89e6b
Michael Mills <mmills6060@gmail.com>
2023-10-12 10:38:51 -0400

    working

--- Motor_Control/Arm/armControl.py
+++ Motor_Control/Arm/armControl.py
@@ -5,5 +5,20 @@ from adafruit_servokit import ServoKit
 # Initialize the ServoKit with the specified I2C bus and address
 kit = ServoKit(channels=16)
 
+
+
+
+
+# Elbow
+# 0 - 180
 kit.servo[2].angle = 90
-kit.servo[1].angle = 90
+
+
+# Wrist
+# 0 - 180 
+kit.servo[3].angle = 90
+
+
+# Shoulder Movement
+# 30 - 180 
+kit.servo[5].angle = 90
