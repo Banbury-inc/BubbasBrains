35ec416593dde516a826b2098382d0fc4f4b8303
Michael Mills <mmills6060@gmail.com>
2023-10-12 11:22:50 -0400

    working

--- Motor_Control/Arm/armControl.py
+++ Motor_Control/Arm/armControl.py
@@ -1,29 +1,5 @@
 import time
 from adafruit_servokit import ServoKit
-
-
-# Initialize the ServoKit with the specified I2C bus and address
-kit = ServoKit(channels=16)
-
-
-elbowangle = 90
-wristangle = 90
-shoulderangle = 90
-
-
-# Elbow
-# 0 - 180
-kit.servo[2].angle = elbowangle
-
-
-# Wrist
-# 0 - 180 
-kit.servo[3].angle = wristangle
-
-
-# Shoulder Movement
-# 30 - 180 
-kit.servo[5].angle = shoulderangle
 def moveshoulderup(shoulderangle):
     while shoulderangle < 30:
         shoulderangle += 1
@@ -56,6 +32,28 @@ def movewristdown(wristangle):
         time.sleep(0.01)
 
 def main():
+
+    print("Starting command")
+    # Initialize the ServoKit with the specified I2C bus and address
+    kit = ServoKit(channels=16)
+
+
+    elbowangle = 120
+    wristangle = 120
+    shoulderangle = 120
+
+    # Elbow
+    # 0 - 180
+    kit.servo[2].angle = elbowangle
+
+    # Wrist
+    # 0 - 180 
+    kit.servo[3].angle = wristangle
+
+    # Shoulder Movement
+    # 30 - 180 
+    kit.servo[5].angle = shoulderangle
+
     moveshoulderup(shoulderangle)
     moveshoulderdown(shoulderangle)
     moveelbowup(elbowangle)
