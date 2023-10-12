12e82ec83894478b512dbebc1eae2788ff4424bb
Michael Mills <mmills6060@gmail.com>
2023-10-12 09:00:14 -0400

    dwdw

--- Motor_Control/Arm/armControl.py
+++ Motor_Control/Arm/armControl.py
@@ -2,9 +2,6 @@
 import time
 from adafruit_servokit import ServoKit
 
-# Connect to the I2C bus and address
-i2c_bus_number = 1  # Adjust this to the correct I2C bus number
-device_address = 0x71  # Adjust this to the correct device address
 
 # Initialize the ServoKit with the specified I2C bus and address
-kit = ServoKit(channels=16, address=device_address, i2c=i2c_bus_number)
+kit = ServoKit(channels=16, address=0x71)
