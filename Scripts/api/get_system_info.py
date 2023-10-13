eb81049b1747469745978a102b5f21e32472e844
Michael Mills <mmills6060@gmail.com>
2023-10-13 09:28:19 -0400

    dwqdwq

---
+++ Scripts/api/get_system_info.py
@@ -0,0 +1,55 @@
+import psutil
+import time
+import subprocess
+class SystemInfo:
+    @staticmethod
+    def get_cpu_usage():
+        return psutil.cpu_percent(interval=1, percpu=True)
+
+    @staticmethod
+    def catch_cpu_usage():
+        while True:
+            cpu_percentages = SystemInfo.get_cpu_usage()
+            for i, cpu_percentage in enumerate(cpu_percentages):
+                print(f"CPU Core {i}: {cpu_percentage}%")
+            time.sleep(1)
+
+    @staticmethod
+    def get_memory_usage():
+        memory_info = psutil.virtual_memory()
+        return {
+            "total": memory_info.total,
+            "available": memory_info.available,
+            "used": memory_info.used,
+            "free": memory_info.free,
+            "percent": memory_info.percent,
+        }
+
+    @staticmethod
+    def catch_memory_usage():
+        while True:
+            memory_usage = SystemInfo.get_memory_usage()
+            print("Memory Usage:")
+            print(f"Total: {memory_usage['total']} bytes")
+            print(f"Available: {memory_usage['available']} bytes")
+            print(f"Used: {memory_usage['used']} bytes")
+            print(f"Free: {memory_usage['free']} bytes")
+            print(f"Percent Used: {memory_usage['percent']}%")
+            time.sleep(5)
+
+    @staticmethod
+    def get_device_name():
+        try:
+            completed_process = subprocess.run(['hostname'], stdout=subprocess.PIPE, text=True, check=True)
+            device_name = completed_process.stdout.strip()
+            return device_name
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
+            return None
+
+def main():
+    name = SystemInfo.get_device_name()
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
--- Scripts/api/projectname/appname/views.py
+++ Scripts/api/projectname/appname/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 import sys
 import os
 from django.shortcuts import render
+from get_system_info import SystemInfo
 
 class ItemListCreateView(generics.ListCreateAPIView):
     queryset = Item.objects.all()
@@ -17,4 +18,7 @@ def index(request):
     return render(request, 'Manual_Control_dev.html')
 def test(request):
     response = "Hello World"
+    return JsonResponse({'result' : response})
+def getSystemInfo(request):
+    response = SystemInfo.get_device_name()
     return JsonResponse({'result' : response})
