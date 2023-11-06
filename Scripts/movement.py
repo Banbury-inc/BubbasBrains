01cad2f46c46b129dad6b30d24c71aa2ff85ff56
mmills6060 <mmills6060@gmail.com>
2023-11-06 15:01:37 -0500

    working

--- Scripts/movement.py
+++ Scripts/movement.py
@@ -1,14 +1,3 @@
-def stop_motors():
-    print("Stopping Motors")
-def move_forward():
-    print("Moving Forward")
-def move_backward():
-    print("Moving Backward")
-def turn_left():
-    print("Turning Left")
-def turn_right():
-    print("Turning Right")
-
 def main():
     print("Hello World")
 if __name__ == '__main__':
